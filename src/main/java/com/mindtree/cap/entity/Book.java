f20fc0ef73d123d7249783f7111ecfec87420e25
Dell <Dell@DESKTOP-F4VLA2B>
2021-07-04 13:26:39 +0530

    commit..

--- src/main/java/com/mindtree/cap/entity/Book.java
+++ src/main/java/com/mindtree/cap/entity/Book.java
@@ -2,4 +2,20 @@ package com.mindtree.cap.entity;
 
 public class Book {
 
+	
+	private String bookname;
+	private int id;
+	public String getBookname() {
+		return bookname;
+	}
+	public void setBookname(String bookname) {
+		this.bookname = bookname;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
 }
